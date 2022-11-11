af200ed50f56e11be4c522385cabf269d34a5352
dtynn <dtynn@163.com>
2022-11-11 21:34:38 +0800

    feat: impl GimbalRecenterAction

--- src/proto/v1/action/gimbal.rs
+++ src/proto/v1/action/gimbal.rs
@@ -4,7 +4,7 @@ use crate::{
         action::{Action, Progress},
         cmd::Command,
         host2byte,
-        v1::ctrl::{GimbalActionPush, GimbalRotate},
+        v1::ctrl::{GimbalActionPush, GimbalRecenter, GimbalRotate},
         Completed,
     },
     util::unit_convertor,
@@ -82,6 +82,65 @@ impl Action for GimbalMoveAction {
             pitch_speed,
             yaw_speed,
             coordinate: self.coordinate as u8,
+            ..Default::default()
+        })
+    }
+
+    fn is_completed(&self) -> bool {
+        self.status.is_completed()
+    }
+
+    fn apply_progress(
+        &mut self,
+        progress: Progress<<Self::Cmd as Command>::Response, Self::Status, Self::Event>,
+    ) -> Result<bool> {
+        match progress {
+            Progress::Response(resp) => {
+                self.status.state = resp.into();
+            }
+
+            Progress::Event(status, evt) => {
+                self.progress.yaw = (evt.yaw as f32) / 10.0;
+                self.progress.pitch = (evt.pitch as f32) / 10.0;
+                self.progress.roll = (evt.roll as f32) / 10.0;
+                self.status = status;
+            }
+        }
+        Ok(self.status.is_completed())
+    }
+}
+
+pub struct GimbalRecenterAction {
+    pitch_speed: u16,
+    yaw_speed: u16,
+
+    pub progress: GimbalMoveProgress,
+    pub status: V1ActionStatus,
+}
+
+impl GimbalRecenterAction {
+    pub fn new(pitch_speed: u16, yaw_speed: u16) -> Self {
+        Self {
+            pitch_speed,
+            yaw_speed,
+
+            progress: Default::default(),
+            status: Default::default(),
+        }
+    }
+}
+
+impl Action for GimbalRecenterAction {
+    type Cmd = GimbalRecenter;
+    type Event = GimbalActionPush;
+    type Status = V1ActionStatus;
+
+    const RECEIVER: u8 = host2byte(4, 0);
+
+    fn pack_cmd(&self) -> Result<Self::Cmd> {
+        Ok(GimbalRecenter {
+            pitch_speed: self.pitch_speed,
+            yaw_speed: self.yaw_speed,
             ..Default::default()
         })
     }
