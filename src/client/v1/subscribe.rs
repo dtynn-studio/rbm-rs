caf117dc460f1cb0b0279e4befb97410e5033215
dtynn <dtynn@163.com>
2022-12-16 17:14:44 +0800

    feat(client::v1): impl Subscriber

--- src/client/v1/subscribe.rs
+++ src/client/v1/subscribe.rs
@@ -1,30 +1,212 @@
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
+use tracing::warn;
+
 use crate::proto::Deserialize;
 use crate::{
-    client::RawHandler,
+    client::{v1::Client as V1Client, Client, RawHandler},
     proto::{
         v1::{
-            subscribe::{PushPeriodMsg, PUSH_PERIOD_MSG_IDENT},
+            subscribe::{
+                PushPeriodMsg, PushPeriodSubject, SubConfig, SubMsg, SubscribeSequence, UnsubMsg,
+                PUSH_PERIOD_MSG_IDENT,
+            },
             Ident, V1,
         },
-        ProtoSubscribe, Raw,
+        ProtoPush, Raw,
+    },
+    util::{
+        chan::{unbounded, Rx, Tx},
+        host2byte,
     },
     Error, Result,
 };
 
 const HANDLER_NAME: &str = "v1::Subscriber";
+const CMD_RECEIVER: u8 = host2byte(9, 0);
 
-pub struct Subscriber {}
+pub struct PushSubscription<S: PushPeriodSubject> {
+    pub rx: Rx<S>,
+    msg_id: u8,
+    handlers: Arc<SubscribeHandlers>,
+    client: Arc<V1Client>,
+}
+
+pub struct EventSubscription {
+    ident: Ident,
+    handlers: Arc<SubscribeHandlers>,
+}
+
+impl EventSubscription {
+    fn unsub(&mut self) -> Result<()> {
+        self.handlers
+            .handlers
+            .lock()
+            .map_err(|_e| Error::Other("handlers poisoned".into()))?
+            .1
+            .remove(&self.ident);
+
+        Ok(())
+    }
+}
+
+impl Drop for EventSubscription {
+    fn drop(&mut self) {
+        if let Err(e) = self.unsub() {
+            warn!(ident = ?self.ident, "event unsub failed: {:?}", e);
+        }
+    }
+}
+
+impl<S: PushPeriodSubject> Drop for PushSubscription<S> {
+    fn drop(&mut self) {
+        if let Err(e) = self.unsub() {
+            warn!(
+                uid = S::UID,
+                msg_id = self.msg_id,
+                "push unsub failed: {:?}",
+                e
+            );
+        }
+    }
+}
+
+impl<S: PushPeriodSubject> PushSubscription<S> {
+    fn unsub(&mut self) -> Result<()> {
+        self.handlers
+            .handlers
+            .lock()
+            .map_err(|_e| Error::Other("handlers poisoned".into()))?
+            .0
+            .remove(&self.msg_id);
+
+        let unsub_msg = UnsubMsg {
+            node_id: self.client.host(),
+            msg_id: self.msg_id,
+            ..Default::default()
+        };
+
+        let _ = self
+            .client
+            .send_cmd(Some(CMD_RECEIVER), unsub_msg, true)?
+            .ok_or_else(|| Error::Other("response required for sub msg".into()))?;
+
+        Ok(())
+    }
+}
+
+pub struct Subscriber {
+    seq: SubscribeSequence,
+    handlers: Arc<SubscribeHandlers>,
+    client: Arc<V1Client>,
+}
+
+impl Drop for Subscriber {
+    fn drop(&mut self) {
+        let _ = self.client.unregister_raw_handler(HANDLER_NAME);
+    }
+}
 
 impl Subscriber {
-    pub fn subscribe_period_push<S: ProtoSubscribe<V1>>(&self) {
-        unimplemented!()
+    pub fn new(client: Arc<V1Client>) -> Result<Self> {
+        let handlers: Arc<SubscribeHandlers> = Default::default();
+
+        client.register_raw_handler(HANDLER_NAME, handlers.clone())?;
+
+        Ok(Subscriber {
+            seq: Default::default(),
+            handlers,
+            client,
+        })
     }
 
-    pub fn subscribe_event(&self) {
-        unimplemented!()
+    pub fn subscribe_period_push<S: PushPeriodSubject>(
+        &self,
+        cfg: Option<SubConfig>,
+    ) -> Result<PushSubscription<S>>
+    where
+        S: PushPeriodSubject + Send + 'static,
+    {
+        let msg_id = self.seq.next();
+        let sub_msg = SubMsg::single(self.client.host(), msg_id, cfg.unwrap_or_default(), S::UID);
+
+        let (mut tx, rx) = unbounded();
+        let hdl: SubscribeHandler = Box::new(move |input| {
+            match input {
+                SubscribeHandlerInput::Data(data) => {
+                    let push = <S as Deserialize<V1>>::de(data)?;
+                    tx.send(push)
+                        .map_err(|_e| Error::Other("push chan broken".into()))?;
+                }
+
+                SubscribeHandlerInput::Check => {
+                    if !tx.is_closed() {
+                        return Err(Error::Other("push chan closed".into()));
+                    }
+                }
+            };
+
+            Ok(())
+        });
+
+        self.handlers
+            .handlers
+            .lock()
+            .map(|mut cbs| cbs.0.insert(msg_id, hdl))
+            .map_err(|_e| Error::Other("handlers poisoned".into()))?;
+
+        let resp = self
+            .client
+            .send_cmd(Some(CMD_RECEIVER), sub_msg, true)?
+            .ok_or_else(|| Error::Other("response required for sub msg".into()))?;
+
+        if !resp.ret.is_ok() {
+            return Err(Error::Other(
+                format!("sub failed with returned info: {:?}", resp).into(),
+            ));
+        }
+
+        Ok(PushSubscription {
+            rx,
+            msg_id,
+            client: self.client.clone(),
+            handlers: self.handlers.clone(),
+        })
+    }
+
+    pub fn subscribe_event<P: ProtoPush<V1>>(&self, mut tx: Tx<P>) -> Result<EventSubscription>
+    where
+        P: ProtoPush<V1> + Send + 'static,
+    {
+        let hdl: SubscribeHandler = Box::new(move |input| {
+            match input {
+                SubscribeHandlerInput::Data(data) => {
+                    let event = <P as Deserialize<V1>>::de(data)?;
+                    tx.send(event)
+                        .map_err(|_e| Error::Other("push chan broken".into()))?;
+                }
+
+                SubscribeHandlerInput::Check => {
+                    if !tx.is_closed() {
+                        return Err(Error::Other("push chan closed".into()));
+                    }
+                }
+            };
+
+            Ok(())
+        });
+
+        self.handlers
+            .handlers
+            .lock()
+            .map(|mut cbs| cbs.1.insert(P::IDENT, hdl))
+            .map_err(|_e| Error::Other("handlers poisoned".into()))?;
+
+        Ok(EventSubscription {
+            ident: P::IDENT,
+            handlers: self.handlers.clone(),
+        })
     }
 }
 
@@ -35,6 +217,7 @@ enum SubscribeHandlerInput<'a> {
 
 type SubscribeHandler = Box<dyn FnMut(SubscribeHandlerInput) -> Result<()> + Send>;
 
+#[derive(Default)]
 struct SubscribeHandlers {
     handlers: Mutex<(
         HashMap<u8, SubscribeHandler>,    // period push msg handlers
--- src/proto/v1/subscribe.rs
+++ src/proto/v1/subscribe.rs
@@ -1,105 +1,35 @@
 use std::io::Write;
+use std::sync::atomic::{AtomicU64, Ordering};
 
 use byteorder::{WriteBytesExt, LE};
 
 use super::{cset::CMD_SET_SUBSCRIBE, impl_v1_cmd, Ident, RetOK, V1};
 use crate::{
-    ensure_buf_size, ensure_ok,
+    ensure_buf_size,
     proto::{Deserialize, ProtoPush, Serialize},
     Error, Result, RetCode,
 };
 
 pub const PUSH_PERIOD_MSG_IDENT: Ident = (CMD_SET_SUBSCRIBE, 0x8);
 
-impl_v1_cmd!(AddNode, AddNodeResp, CMD_SET_SUBSCRIBE, 0x01);
+pub const RM_SDK_FIRST_DDS_ID: u8 = 20;
+pub const RM_SDK_LAST_DDS_ID: u8 = 255;
 
-#[derive(Debug)]
-pub struct AddNodeResp {
-    pub pub_node_id: u8,
-}
+const DDS_SEQ_MOD: u64 = (RM_SDK_LAST_DDS_ID - RM_SDK_FIRST_DDS_ID) as u64;
 
-impl Deserialize<V1> for AddNodeResp {
-    fn de(buf: &[u8]) -> Result<Self> {
-        ensure_buf_size!(buf, 1);
-        let retcode: RetCode = buf[0].into();
-        if retcode.0 == 0 || retcode.0 == 0x50 {
-            ensure_buf_size!(buf, 2);
-            Ok(AddNodeResp {
-                pub_node_id: buf[1],
-            })
-        } else {
-            Err(Error::NotOK {
-                code: retcode,
-                errcode: None,
-                msg: None,
-            })
-        }
+#[derive(Default)]
+pub struct SubscribeSequence(AtomicU64);
+
+impl SubscribeSequence {
+    pub fn next(&self) -> u8 {
+        let next = self.0.fetch_add(1, Ordering::Relaxed);
+        RM_SDK_FIRST_DDS_ID + (next % DDS_SEQ_MOD) as u8
     }
 }
 
-#[derive(Debug)]
-pub struct AddNode {
-    pub node_id: u8,
-    pub sub_vision: u32,
-}
-
-impl Default for AddNode {
-    fn default() -> Self {
-        Self {
-            node_id: 0,
-            sub_vision: 0x03000000,
-        }
-    }
-}
-
-impl Serialize<V1> for AddNode {
-    const SIZE_HINT: usize = 5;
-
-    fn ser(&self, w: &mut impl Write) -> Result<()> {
-        w.write_u8(self.node_id)?;
-        w.write_u32::<LE>(self.sub_vision)?;
-        Ok(())
-    }
-}
-
-impl_v1_cmd!(SubNodeReset, RetOK, CMD_SET_SUBSCRIBE, 0x02);
-
-#[derive(Debug, Default)]
-pub struct SubNodeReset {
-    pub node_id: u8,
-}
-
-impl Serialize<V1> for SubNodeReset {
-    const SIZE_HINT: usize = 1;
-    fn ser(&self, w: &mut impl Write) -> Result<()> {
-        w.write_u8(self.node_id).map_err(From::from)
-    }
-}
-
-impl_v1_cmd!(SubMsg, SubMsgResp, CMD_SET_SUBSCRIBE, 0x03);
-
-#[derive(Debug)]
-pub struct SubMsgResp {
-    pub pub_node_id: u8,
-    pub ack_sub_mode: u8,
-    pub ack_msg_id: u8,
-    pub ack_err_uid_data: u64,
-}
-
-impl Deserialize<V1> for SubMsgResp {
-    fn de(buf: &[u8]) -> Result<Self> {
-        ensure_ok!(buf);
-        ensure_buf_size!(buf, 8);
-        Ok(Self {
-            pub_node_id: buf[1],
-            ack_sub_mode: buf[2],
-            ack_msg_id: buf[3],
-            ack_err_uid_data: buf[4] as u64
-                | (buf[5] as u64) << 8
-                | (buf[6] as u64) << 16
-                | (buf[7] as u64) << 24,
-        })
-    }
+#[derive(Default)]
+pub struct SubConfig {
+    pub freq: SubFreq,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -110,6 +40,43 @@ pub enum SubFreq {
     TenHz = 10,
     TwentyHz = 20,
     FiftyHz = 50,
+}
+
+impl Default for SubFreq {
+    fn default() -> Self {
+        Self::OneHz
+    }
+}
+
+pub trait PushPeriodSubject: Deserialize<V1> {
+    const UID: u64;
+}
+
+impl_v1_cmd!(SubMsg, SubMsgResp, CMD_SET_SUBSCRIBE, 0x03);
+
+#[derive(Debug)]
+pub struct SubMsgResp {
+    pub ret: RetCode,
+    pub pub_node_id: u8,
+    pub ack_sub_mode: u8,
+    pub ack_msg_id: u8,
+    pub ack_err_uid_data: u64,
+}
+
+impl Deserialize<V1> for SubMsgResp {
+    fn de(buf: &[u8]) -> Result<Self> {
+        ensure_buf_size!(buf, 8);
+        Ok(Self {
+            ret: RetCode::from(buf[0]),
+            pub_node_id: buf[1],
+            ack_sub_mode: buf[2],
+            ack_msg_id: buf[3],
+            ack_err_uid_data: buf[4] as u64
+                | (buf[5] as u64) << 8
+                | (buf[6] as u64) << 16
+                | (buf[7] as u64) << 24,
+        })
+    }
 }
 
 #[derive(Debug)]
@@ -124,12 +91,12 @@ pub struct SubMsg {
 }
 
 impl SubMsg {
-    pub fn single(node_id: u8, msg_id: u8, freq: SubFreq, uid: u64) -> Self {
+    pub fn single(node_id: u8, msg_id: u8, cfg: SubConfig, uid: u64) -> Self {
         Self {
             node_id,
             msg_id,
             sub_uid_list: vec![uid],
-            sub_freq: freq,
+            sub_freq: cfg.freq,
             ..Default::default()
         }
     }
@@ -174,7 +141,7 @@ impl Serialize<V1> for SubMsg {
 
 impl_v1_cmd!(UnsubMsg, RetOK, CMD_SET_SUBSCRIBE, 0x04);
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct UnsubMsg {
     pub sub_mode: u8,
     pub node_id: u8,
@@ -212,8 +179,67 @@ impl ProtoPush<V1> for PushPeriodMsg {
     const IDENT: Ident = PUSH_PERIOD_MSG_IDENT;
 }
 
-pub trait PushPeriodRequest {
-    const UID: u64;
+impl_v1_cmd!(SubNodeAdd, SubNodeAddResp, CMD_SET_SUBSCRIBE, 0x01);
 
-    type Push: Deserialize<V1>;
+#[derive(Debug)]
+pub struct SubNodeAddResp {
+    pub pub_node_id: u8,
+}
+
+impl Deserialize<V1> for SubNodeAddResp {
+    fn de(buf: &[u8]) -> Result<Self> {
+        ensure_buf_size!(buf, 1);
+        let retcode: RetCode = buf[0].into();
+        if retcode.0 == 0 || retcode.0 == 0x50 {
+            ensure_buf_size!(buf, 2);
+            Ok(SubNodeAddResp {
+                pub_node_id: buf[1],
+            })
+        } else {
+            Err(Error::NotOK {
+                code: retcode,
+                errcode: None,
+                msg: None,
+            })
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct SubNodeAdd {
+    pub node_id: u8,
+    pub sub_vision: u32,
+}
+
+impl Default for SubNodeAdd {
+    fn default() -> Self {
+        Self {
+            node_id: 0,
+            sub_vision: 0x03000000,
+        }
+    }
+}
+
+impl Serialize<V1> for SubNodeAdd {
+    const SIZE_HINT: usize = 5;
+
+    fn ser(&self, w: &mut impl Write) -> Result<()> {
+        w.write_u8(self.node_id)?;
+        w.write_u32::<LE>(self.sub_vision)?;
+        Ok(())
+    }
+}
+
+impl_v1_cmd!(SubNodeReset, RetOK, CMD_SET_SUBSCRIBE, 0x02);
+
+#[derive(Debug, Default)]
+pub struct SubNodeReset {
+    pub node_id: u8,
+}
+
+impl Serialize<V1> for SubNodeReset {
+    const SIZE_HINT: usize = 1;
+    fn ser(&self, w: &mut impl Write) -> Result<()> {
+        w.write_u8(self.node_id).map_err(From::from)
+    }
 }
