53697f865bd2703d3dfc73458bb4bf0f92041061
dtynn <dtynn@163.com>
2023-11-05 14:40:51 +0800

    feat(structpack): impl Pack & Unpack for slices

--- rbm-structpack/src/lib.rs
+++ rbm-structpack/src/lib.rs
@@ -18,6 +18,34 @@ pub trait Unpack: Sized {
     fn unpack<T: ByteOrder>(&mut self, r: &mut impl Read) -> Result<()>;
 }
 
+impl<E: Pack> Pack for &[E] {
+    fn size(&self) -> Option<usize> {
+        let mut size = 0;
+        for v in &self[..] {
+            size += v.size()?;
+        }
+
+        Some(size)
+    }
+
+    fn pack<T: ByteOrder>(&self, w: &mut impl Write) -> Result<()> {
+        for v in &self[..] {
+            v.pack::<T>(w)?;
+        }
+        Ok(())
+    }
+}
+
+impl<E: Unpack> Unpack for &mut [E] {
+    fn unpack<T: ByteOrder>(&mut self, r: &mut impl Read) -> Result<()> {
+        for v in &mut self[..] {
+            v.unpack::<T>(r)?;
+        }
+
+        Ok(())
+    }
+}
+
 pub mod preclude {
     pub use super::{Pack, Unpack};
     pub use byteorder::{ByteOrder, ReadBytesExt, WriteBytesExt, BE, LE};
